228408e540c7911a5a20aff656bbb58131eb11be
nobodyPerfecZ <dennis-jabs@outlook.de>
2025-03-07 15:50:49 +0100

    D. J.: - Added the python scripts into the examples folder

--- examples/scripts/download.py
+++ examples/scripts/download.py
@@ -3,11 +3,15 @@
 from manim_stock.util import download_stock_data, preprocess_stock_data
 
 if __name__ == "__main__":
-    # Download stock data
+   # Download stock data
     df = download_stock_data(
         tickers=["AAPL", "NVDA", "TSLA"],
         start="1900-01-01",
         end="2100-01-01",
     )
-    df = preprocess_stock_data(df)
+
+    # Preprocess stock data
+    df = preprocess_stock_data(df, column="High")
+
+    # Safe stock data as CSV file
     df.to_csv("stock_data.csv", index=False)
